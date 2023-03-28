01594c67ba24e0f69b7a3dd4bd7bf65ae7fb8cc9
PHANLAMANH <93565802+PHANLAMANH@users.noreply.github.com>
2023-03-28 15:10:04 +0700

    update data
    create data.txt & data.json

--- GUI.py
+++ GUI.py
@@ -1,40 +1,35 @@
 import tkinter as tk
 from tkinter import ttk
+import NTFS
+import os
+import FAT32
+import json
 
-data = {
-    "filename": {
-        "type": "file",
-        "size": 1234,
-        "created": "2022-01-01 12:34:56",
-        "modified": "2022-02-02 23:45:01",
-    },
-    "foldername": {
-        "type": "folder",
-        "created": "2022-01-01 12:34:56",
-        "modified": "2022-02-02 23:45:01",
-        "contents": {
-            "filename": {
-                "type": "file",
-                "size": 5678,
-                "created": "2022-01-02 12:34:56",
-                "modified": "2022-02-03 23:45:01",
-            },
-            "subfoldername": {
-                "type": "folder",
-                "created": "2022-01-03 12:34:56",
-                "modified": "2022-02-04 23:45:01",
-                "contents": {
-                    "filename": {
-                        "type": "file",
-                        "size": 91011,
-                        "created": "2022-01-04 12:34:56",
-                        "modified": "2022-02-05 23:45:01",
-                    }
-                },
-            },
-        },
-    },
-}
+
+def import_data(file_path):
+    with open(file_path, "r") as file:
+        data = json.load(file)
+    return data
+
+
+data = import_data("data.txt")
+
+
+# Load the data from data.txt into a Python dictionary
+def read_data():
+    with open("data.txt", "r") as f:
+        data = eval(f.read())
+    return data
+
+
+data = read_data()
+
+# Convert the Python dictionary to a JSON-formatted string
+json_data = json.dumps(data)
+
+# Write the JSON-formatted string to a new file
+with open("data.json", "w") as f:
+    f.write(json_data)
 
 
 def add_items(parent, items):
@@ -42,7 +37,8 @@ def add_items(parent, items):
         tags = ()
         if value.get("type") == "folder":
             tags = ("folder",)
-
+        if value.get("type") == "file":
+            tags = ("file",)
         node = tree.insert(
             parent,
             "end",
@@ -59,6 +55,20 @@ def add_items(parent, items):
             add_items(node, value.get("contents", {}))
 
     # Configure the font for the treeview item's text
+    tree.tag_configure(
+        "folder",
+        font=(
+            "Segoe UI",
+            12,
+        ),
+    )
+    tree.tag_configure(
+        "file",
+        font=(
+            "Segoe UI",
+            12,
+        ),
+    )
 
 
 # Create the GUI window
@@ -69,20 +79,25 @@ root.geometry("1000x300")
 # Create a frame for the treeview widget
 frame = ttk.Frame(root)
 frame.pack(fill=tk.BOTH, expand=1)
+# modify the frame to add a scrollbar
+frame.grid_rowconfigure(0, weight=1)
+frame.grid_columnconfigure(0, weight=1)
+
 
 # Create a treeview widget
 tree = ttk.Treeview(
-    frame, columns=("Type", "Size", "Created", "Modified"), selectmode="browse"
+    frame,
+    columns=("Type", "Size", "Date Created", "Date Modified"),
+    selectmode="browse",
 )
 tree.pack(fill=tk.BOTH, expand=1)
 # Create a custom style for the heading
 style = ttk.Style(root)
-style.configure("Treeview.Item", font=("Segoe UI", 20))
 
 style.configure(
     "Treeview.Heading",
-    background="#3366ff",
-    foreground="black",
+    background="#00FFFF",
+    foreground="darkblue",
     font=(
         "Segoe UI",
         14,
@@ -94,8 +109,8 @@ style.configure(
 tree.heading("#0", text="Name", anchor=tk.W)
 tree.heading("#1", text="Type", anchor=tk.W)
 tree.heading("#2", text="Size", anchor=tk.W)
-tree.heading("#3", text="Created", anchor=tk.W)
-tree.heading("#4", text="Modified", anchor=tk.W)
+tree.heading("#3", text="Date Created", anchor=tk.W)
+tree.heading("#4", text="Date Modified", anchor=tk.W)
 
 # Set the column widths
 tree.column("#0", minwidth=200, width=400)
@@ -110,7 +125,150 @@ scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 tree.configure(yscrollcommand=scrollbar.set)
 
 
+# Sort the treeview items by clicking on the column headings (optional)
+def sort_column(tree, col, reverse):
+    # Get the list of items in the tree
+    items = [(tree.set(child, col), child) for child in tree.get_children("")]
+
+    # Sort the items based on the column value
+    items.sort(reverse=reverse)
+
+    for index, (val, child) in enumerate(items):
+        # Re-order the items in the tree
+        tree.move(child, "", index)
+
+    # Reverse the sort direction for the next click
+    tree.heading(col, command=lambda: sort_column(tree, col, not reverse))
+
+
+# Set the command for each column heading to sort the items
+tree.heading(
+    "#0", text="Name", anchor=tk.W, command=lambda: sort_column(tree, "#0", False)
+)
+tree.heading(
+    "#1", text="Type", anchor=tk.W, command=lambda: sort_column(tree, "#1", False)
+)
+tree.heading(
+    "#2", text="Size", anchor=tk.W, command=lambda: sort_column(tree, "#2", False)
+)
+tree.heading(
+    "#3",
+    text="Date Created",
+    anchor=tk.W,
+    command=lambda: sort_column(tree, "#3", False),
+)
+tree.heading(
+    "#4",
+    text="Date Modified",
+    anchor=tk.W,
+    command=lambda: sort_column(tree, "#4", False),
+)
+
+
+# create class for menu items to be added to the right click menu (optional)
+class Menu:
+    def __init__(self, text, command):
+        self.text = text
+        self.command = command
+
+
+# Create Menu for right click on treeview item (optional)
+def on_right_click(event):
+    # Select the item under the cursor
+    item = tree.identify("item", event.x, event.y)
+    tree.selection_set(item)
+
+    # Create a command create a new folder (optional)
+    def new_folder():
+        tree.insert(
+            item,
+            "end",
+            text="New Folder",
+            values=["folder", "", "", ""],
+            tags=("folder",),
+        )
+
+    # convert the treeview item to a dictionary
+    def treeview_to_dict(tree):
+        d = {tree.item(item)["text"]: {} for item in tree.get_children()}
+        for key, value in d.items():
+            # get the children of the item
+            children = tree.get_children(item)
+            if children:
+                d[key] = treeview_to_dict(tree)
+        return d
+
+    # Create a command to delete the selected item (optional)
+    def delete_item():
+        tree.delete(item)
+
+    # Create a command to rename the selected item (optional)
+
+    # Create a command to create a new file (optional)
+    def new_file():
+        tree.insert(
+            item,
+            "end",
+            text="New File",
+            values=["file", "", "", ""],
+            tags=("file",),
+        )
+
+    def rename_item():
+        tree.edit(item)
+
+    # create a command to show the properties of the selected item (optional)
+    def show_properties():
+        print("Properties")
+
+    # Display the menu
+    menu = tk.Menu(root, tearoff=0)
+    menu.add_command(label="New Folder", command=new_folder)
+    menu.add_command(label="New File", command=new_file)
+
+    menu.add_command(label="Delete", command=delete_item)
+    menu.add_command(label="Properties", command=show_properties)
+
+    menu.post(event.x_root, event.y_root)
+
+    # Save the input data to data_demo.py (optional)
+
+
+# Bind the right-click event to the treeview widget
+tree.bind("<Button-3>", on_right_click)
+
+
+def get_items(tree, parent):
+    # Get the data from the treeview
+    items = []
+    for item in parent:
+        # Get the item's text and values
+        text = tree.item(item, "text")
+        values = tree.item(item, "values")
+        # Get the item's children
+        children = tree.get_children(item)
+        # Create a dictionary for the item's data
+        item_data = {
+            "text": text,
+            "values": values,
+            "contents": get_items(tree, children),
+        }
+        # Add the item's data to the list
+        items.append(item_data)
+    return items
+
+
+# convert the treeview to a dictionary and save it to data_demo.py (optional)
+def save_data():
+    # Get the data from the treeview
+    data = get_items(tree, tree.get_children())
+    # Save the data to the filen
+    with open("data_demo.py", "w") as f:
+        f.write("data = " + str(data))
+
+
 # Add the data to the tree
 add_items("", data)
+
 
 root.mainloop()
--- demo.py
+++ demo.py
@@ -1,3 +1,64 @@
 import os
+import tkinter as tk
+from tkinter import ttk
 
 
+class FileExplorer:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("File Explorer")
+
+        # Create the Treeview widget
+        self.treeview = ttk.Treeview(root)
+
+        # Define the columns
+        self.treeview["columns"] = ("size", "type", "modified")
+
+        # Format the columns
+        self.treeview.column("#0", width=250, stretch=tk.YES)
+        self.treeview.column("size", width=100, stretch=tk.NO)
+        self.treeview.column("type", width=100, stretch=tk.NO)
+        self.treeview.column("modified", width=150, stretch=tk.NO)
+
+        # Create the headings
+        self.treeview.heading("#0", text="File Name", anchor=tk.W)
+        self.treeview.heading("size", text="File Size", anchor=tk.W)
+        self.treeview.heading("type", text="File Type", anchor=tk.W)
+        self.treeview.heading("modified", text="Modified Date", anchor=tk.W)
+
+        # Add the root node
+        self.root_node = self.treeview.insert("", "end", text="C:/", open=True)
+
+        # Populate the Treeview
+        self.populate_tree(self.root_node)
+
+        # Add the Treeview to the GUI
+        self.treeview.pack(expand=tk.YES, fill=tk.BOTH)
+
+    def populate_tree(self, parent):
+        # Get the directory contents
+        path = self.treeview.set(parent, "#0")
+        for item in os.listdir(path):
+            # Get the item path
+            item_path = os.path.join(path, item)
+
+            # Determine if the item is a directory
+            isdir = os.path.isdir(item_path)
+
+            # Insert the item into the Treeview
+            if isdir:
+                item_id = self.treeview.insert(parent, "end", text=item, open=False)
+                self.populate_tree(item_id)
+            else:
+                size = os.path.getsize(item_path)
+                typ = os.path.splitext(item)[1]
+                modified = os.path.getmtime(item_path)
+                self.treeview.insert(
+                    parent, "end", text=item, values=(size, typ, modified)
+                )
+
+
+# Create the GUI
+root = tk.Tk()
+file_explorer = FileExplorer(root)
+root.mainloop()
