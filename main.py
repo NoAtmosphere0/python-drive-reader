39cefb471aeaea56ceba22f4d433ddfd3f67b85e
Đặng Hoàng Long <tylbam11@gmail.com>
2023-03-28 15:24:42 +0700

    disk_info.py now returns a dictionary

--- main.py
+++ main.py
@@ -8,23 +8,22 @@ def main():
     ntfs_path = r"bin_files\bin_NTFS_Partition1.bin" 
     fat32_path = r"bin_files\bin_FAT32_Partition2.bin"
     disk_info_path = r"bin_files\bin_disk_information.bin"
+    physical_drive = r"\\.\PhysicalDrive1"
     #! This path should be PhysicalDrive1 in the final version
 
     # master_boot_record = disk_info.MBR(drive)
     # master_boot_record.printMBR()
 
     """"Testing the partition table class"""
-    # pt = disk_info.partition_table()
-    # pt.readPT(disk_info_path)
-    # pt.print_partitions()
+    pt = disk_info.MBR(disk_info_path)
 
     # pt.read(drive)
 
     """"Testing the NTFS class"""
     ntfs = NTFS.NTFS(0, ntfs_path)
-    ntfs.readVBR()
     ntfs.readMFT()
-    ntfs.print_entries()
+    
+    #ntfs.print_entries()
 
 
 
