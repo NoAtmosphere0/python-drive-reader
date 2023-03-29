22d1673ff6b84092b6e929d81c39df75deaeb0a5
RealS0nne <106177822+RealS0nne@users.noreply.github.com>
2023-03-30 00:32:40 +0700

    update 1 more time to FAT32.py

--- FAT32.py
+++ FAT32.py
@@ -1,44 +1,23 @@
+import datetime
+import struct
+import time
+
 class FAT32:
-    def __init__(self)->None:
+    def __init__(self, starting_offset, drive)->None:
+        self.drive_path = drive
+        self.info_dict = {}
+        self.starting_offset = starting_offset
+        self.byte = 0
         #Boot Sector FAT32
         self.BPB_BytesPerSec = 0 #2 bytes
         self.BPB_SecPerClus = 0 #1 byte
         self.BPB_RsvdSecCnt = 0 #2 bytes
         self.BPB_NumFATs = 0 #1 byte
+        self.BPB_HiddSec = 0 #4 bytes
         self.BPB_totSec32 = 0 #4 bytes
         self.BPB_SecsPerFAT = 0 #4 bytes
         self.BPB_RootClus = 0 #4 bytes
         self.BPB_FSInfo = 0 #8 bytes
-<<<<<<< Updated upstream
-        #primary entry
-        self.PE_status = 0 #1 byte
-        self
-    def readBootSector(self, drive):
-        with open(drive, 'rb') as fp:
-            fp.read(11)
-            self.BPB_BytesPerSec = int.from_bytes(fp.read(2), byteorder='little')
-            self.BPB_SecPerClus = int.from_bytes(fp.read(1), byteorder='little')
-            self.BPB_RsvdSecCnt = int.from_bytes(fp.read(2), byteorder='little')
-            self.BPB_NumFATs = int.from_bytes(fp.read(1), byteorder='little')
-            fp.seek(15,1)
-            self.BPB_totSec32 = int.from_bytes(fp.read(4), byteorder='little')
-            self.BPB_SecsPerFAT = int.from_bytes(fp.read(4), byteorder='little')
-            fp.seek(4,1)
-            self.BPB_RootClus = int.from_bytes(fp.read(4), byteorder='little')
-            fp.seek(34,1)
-            self.BPB_FSInfo = fp.read(8).decode('utf-8')
-        return 0
-    def printBootSector(self):
-        print("Bytes per sector: ", self.BPB_BytesPerSec)
-        print("Sectors per cluster: ", self.BPB_SecPerClus)
-        print("Reserved sectors: ", self.BPB_RsvdSecCnt)
-        print("Number of FATs: ", self.BPB_NumFATs)
-        print("Total sectors: ", self.BPB_totSec32)
-        print("Sectors per FAT: ", self.BPB_SecsPerFAT)
-        print("Root cluster: ", self.BPB_RootClus)
-        print("FSInfo: ", self.BPB_FSInfo)
-        return 0
-=======
         #Entry FAT32
         self.DIR_Name = '' #11 bytes
         self.DIR_Attr = 'type' #1 byte
@@ -114,7 +93,7 @@ class FAT32:
                                     check = False
                                 tempName += temp
                             tempData[tempName] = tempName    
-                            print(tempData['ONGNOITENDAIVCL'])                       
+                            print(tempData[tempName])                       
                         else:
                             self.DIR_Name = (self.byte[i:i+8]).decode('utf-8')
                         self.DIR_Attr = 'Directory'
@@ -220,14 +199,8 @@ class FAT32:
                         ################
                     else:
                         continue
->>>>>>> Stashed changes
 
-#bst = FAT32()
-#read from FAT32 partition
-#bst.readBootSector(r"C:\Users\pkhoa\Documents\Operating System\Partition data-20230316T151812Z-001\Partition data\bin_FAT32_Partition2.bin")
 
-<<<<<<< Updated upstream
-=======
     def Entry(self)->None:
         temp_dict = {'DIR_Name': self.DIR_Name, 
                     'DIR_Attr': self.DIR_Attr, 
@@ -259,4 +232,3 @@ RDET = (DRIVE.BootSector()['BPB_RsvdSecCnt'] + DRIVE.BootSector()['BPB_NumFATs']
 DRIVE.readFirstEntry(RDET)
 DATA = DRIVE.Entry()
 #print(DATA)
->>>>>>> Stashed changes
