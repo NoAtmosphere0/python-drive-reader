3e637c3f42bb813bd666dae440f38c95707a7e8c
RealS0nne <106177822+RealS0nne@users.noreply.github.com>
2023-03-30 05:06:33 +0700

    Finish 90% FAT32

--- FAT32.py
+++ FAT32.py
@@ -13,23 +13,19 @@ class FAT32:
         self.BPB_SecPerClus = 0 #1 byte
         self.BPB_RsvdSecCnt = 0 #2 bytes
         self.BPB_NumFATs = 0 #1 byte
-        self.BPB_HiddSec = 0 #4 bytes
         self.BPB_totSec32 = 0 #4 bytes
         self.BPB_SecsPerFAT = 0 #4 bytes
         self.BPB_RootClus = 0 #4 bytes
-        self.BPB_FSInfo = 0 #8 bytes
         #Entry FAT32
         self.DIR_Name = '' #11 bytes
-        self.DIR_Attr = '' #1 byte
-        self.DIR_CrtTimeTenth = 0 #1 byte
-        self.DIR_CrtTime = 0 #2 bytes
-        self.DIR_CrtDate = 0 #2 bytes
-        self.DIR_LstAccDate = 0 #2 bytes
-        self.DIR_FstClusHI = 0 #2 bytes
-        self.DIR_WrtTime = 0 #2 bytes
-        self.DIR_WrtDate = 0 #2 bytes
-        self.DIR_FstClusLO = 0 #2 bytes
+        self.DIR_Attr = 'type' #1 byte
+        self.DIR_CrtTime = '' #2 bytes
+        self.DIR_CrtDate = '' #2 bytes
+        self.DIR_DateCrt = 'Date Created'
+        self.DIR_DateMod = 'Date Modified'
         self.DIR_FileSize = 0 #4 bytes
+        self.DIR_CurClus = 0 #4 bytes
+        self.DIR_NxtClus = 0 #4 bytes
     
     def readBootSector(self)->None:
         with open(self.drive_path, 'rb') as f:
@@ -43,15 +39,20 @@ class FAT32:
             self.BPB_SecsPerFAT = int.from_bytes(self.byte[36:40], byteorder='little')
             self.BPB_RootClus = int.from_bytes(self.byte[44:48], byteorder='little')
             self.BPB_FSInfo = int.from_bytes(self.byte[48:52], byteorder='little')
-    def readFirstEntry(self, offset:hex)->None:
+
+    def detectDIR(self, offset:hex, startingOffset, root):
         with open(self.drive_path, 'rb') as f:
             f.seek(offset)
             self.byte = f.read(512)
-            for i in range (128, 512, 32):
+            number = 0
+            sub = {}
+            for i in range (startingOffset, 512, 32):
                 if (self.byte[i] == 0xE5 or self.byte[i] == 0x00):
                     continue
                 else:
+                    tempData = {}
                     if (self.byte[i+11] == 0x10): #Directory
+                        self.DIR_Name = ''
                         j = i + 11
                         if (self.byte[j - 32] == 0x0F):
                             j -= 32
@@ -93,10 +94,9 @@ class FAT32:
                                             break
                                         temp += (self.byte[j + k - index:j + k + 2 - index]).decode('utf-8')
                                     check = False
-                                self.DIR_Name += temp                                   
+                                self.DIR_Name += temp           
                         else:
-                            self.DIR_Name = (self.byte[i:i+8]).decode('utf-8')
-                        self.DIR_Attr = 'Directory'
+                            self.DIR_Name = (self.byte[i:i+8]).decode('utf-8').rstrip()
                         #created time
                         temp = self.byte[i+13:i+16]
                         reversed_temp = temp[::-1]
@@ -120,7 +120,27 @@ class FAT32:
                         self.DIR_CrtDate = datetime.date(year, month, day)
                         self.DIR_CrtDate = self.DIR_CrtDate.strftime('%m/%d/%Y') #converted to mdy
                         ################
+                        #next cluster
+                        temp = self.byte[i+26:i+27]
+                        binary_string = ''.join(f'{byte:08b}' for byte in temp)
+                        int_value = int(binary_string, 2)
+                        self.DIR_NxtClus = int_value
+                        ################
+                        tempData['name'] = self.DIR_Name
+                        tempData['type'] = 'folder'
+                        tempData['created'] = self.DIR_CrtDate + ' ' + self.DIR_CrtTime
+                        tempData['modified'] = None
+                        if (self.DIR_NxtClus != 0):
+                            tempData.update({'contents' : self.detectDIR(offset + (self.DIR_NxtClus - root) * self.BPB_BytesPerSec * self.BPB_SecPerClus, 64, self.DIR_NxtClus)})
+                            if (tempData['contents'] == {}):
+                                tempData.update({'contents' : None})
+                        else:
+                            tempData.update({'contents' : None})
+                            return
+                        sub.update({number:tempData})
+                        number += 1
                     elif (self.byte[i+11] == 0x20): #File
+                        self.DIR_Name = ''
                         j = i + 11
                         if (self.byte[j - 32] == 0x0F):
                             j -= 32
@@ -162,9 +182,9 @@ class FAT32:
                                             break
                                         temp += (self.byte[j + k - index:j + k + 2 - index]).decode('utf-8')
                                     check = False
-                                self.DIR_Name += temp                                   
+                                self.DIR_Name += temp                          
                         else:
-                            self.DIR_Name = (self.byte[i:i+8]).decode('utf-8') + '.' + (self.byte[i+8:i+11]).decode('utf-8')
+                            self.DIR_Name = (self.byte[i:i+8]).decode('utf-8').rstrip() + '.' + (self.byte[i+8:i+11]).decode('utf-8')
                         self.DIR_Attr = 'File'
                         #created time
                         temp = self.byte[i+13:i+16]
@@ -196,17 +216,25 @@ class FAT32:
                         int_value = int(binary_string, 2)
                         self.DIR_FileSize = int_value
                         ################
+                        tempData['name'] = self.DIR_Name
+                        tempData['type'] = 'file'
+                        tempData['created'] = self.DIR_CrtDate + ' ' + self.DIR_CrtTime
+                        tempData['modified'] = None
+                        tempData['size'] = self.DIR_FileSize
+                        sub.update({number:tempData})
+                        number += 1
                     else:
                         continue
-
+                    # self.info_dict.update({number: tempData})
+                    # number = number + 1
+        return sub
+    def FindDirectory(self, offset:hex, Cluster:hex)->None:
+        if (Cluster == 0x2):
+            for i in range(0, len(self.detectDIR(offset, 128, self.BPB_RootClus)), 1):
+                self.info_dict.update({i: self.detectDIR(offset, 128, self.BPB_RootClus)[i]})
 
     def Entry(self)->None:
-        temp_dict = {'DIR_Name': self.DIR_Name, 
-                    'DIR_Attr': self.DIR_Attr, 
-                    'DIR_CrtTime': self.DIR_CrtTime, 
-                    'DIR_CrtDate': self.DIR_CrtDate, 
-                    'DIR_FileSize': self.DIR_FileSize}
-        return temp_dict
+        return self.info_dict
     def BootSector(self):
         temp_dict = {
                 'StartingOffset': self.starting_offset,
@@ -216,18 +244,18 @@ class FAT32:
                 'BPB_NumFATs': self.BPB_NumFATs,
                 'BPB_TotSec32': self.BPB_totSec32,
                 'BPB_SecsPerFAT':self.BPB_SecsPerFAT,
-                'BPB_NumHeads': self.BPB_RootClus,
-                'BPB_HiddSec': self.BPB_HiddSec
+                'BPB_RootClus': self.BPB_RootClus,
                 }
         return temp_dict
 
-DRIVE = FAT32(0, '\\\\.\F:')
-
+DRIVE = FAT32(0, '\\\\.\G:')
 DRIVE.readBootSector()
 
+
+print('-------------------------------------------------------')
 RDET = (DRIVE.BootSector()['BPB_RsvdSecCnt'] + DRIVE.BootSector()['BPB_NumFATs'] * DRIVE.BootSector()['BPB_SecsPerFAT'] 
 + DRIVE.BootSector()['StartingOffset']) * DRIVE.BootSector()['BPB_BytesPerSec']
 
-DRIVE.readFirstEntry(RDET)
+DRIVE.FindDirectory(RDET, DRIVE.BootSector()['BPB_RootClus'])
 DATA = DRIVE.Entry()
 print(DATA)
